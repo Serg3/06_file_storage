26dca3a04db6a64ee7c043e713aef2f465f42461
kovalov <kovalov-ext@fidor.com>
2021-06-11 10:06:17 +0200

    implement file storage

---
+++ cmd/srch/srch.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go_core/02_search_engine/pkg/crawler/spider"
+	"go_core/02_search_engine/pkg/index"
+	"log"
+)
+
+// Struct 'search' is used
+// for a more convenient representation
+// of search's parameters
+type search struct {
+	scanner *spider.Service
+	sites   []string
+	depth   int
+	storage *index.Storage
+}
+
+func main() {
+	param := flag.String("s", "", "word for search")
+	flag.Parse()
+	if *param != "" {
+		docs := scan().Search(param)
+		fmt.Printf("Search results:\n\n")
+		for _, d := range docs {
+			fmt.Println(d)
+		}
+	} else {
+		flag.PrintDefaults()
+	}
+}
+
+// Function scan() uses package 'crawler'
+// to search through Go sites by word
+// and returs sorted *index.Storage result
+func scan() *index.Storage {
+	s := new()
+
+	if s.storage.Empty() {
+		for _, url := range s.sites {
+			res, err := s.scanner.Scan(url, s.depth)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			s.storage.Append(res)
+		}
+		s.storage.Save()
+	}
+
+	s.storage.Index()
+	s.storage.Sort()
+
+	return s.storage
+}
+
+func new() *search {
+	s := search{}
+	s.sites = []string{"https://go.dev", "https://golang.org/"}
+	s.depth = 2
+	s.scanner = spider.New()
+	s.storage = index.New()
+	return &s
+}
